a47932cb3ddf9c60104692895f794457fabc3804
Lucas Gomes <lulinux2109@gmail.com>
2020-03-24 22:28:09 -0300

    Projeto Pessoal TecBlog

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TecBlog - O seu blog mais atualizado</title>
+    <!-- Links -->
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" />
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,700&display=swap" rel="stylesheet" />
+</head>
+<body>
+    <div class="area-cabecalho">
+            <div class="area-logo">
+                <h1>Daniela<span class="color-text">Oliveira</span></h1>
+            </div>
+            <!-- Fim Área Logo -->
+            <div class="area-menu">
+                <a href="index.php">Home</a>
+                <a href="jogos.php">Jogos</a>
+                <a href="celulares.php">Celulares</a>
+                <a href="informatica.php">Informática</a>
+                <a href="eletrônicos.php">Eletrônicos</a>
+            </div>
+            <!-- Fim Área Menu -->
+    </div>
+    <!-- Fim Área cabeçalho -->
+    <div class="area-principal">
+       <div class="area-postagens">
+           <div class="postagens">
+               <h2>Curso De Redação: Aula - 1</h2>
+               <img src="img/imagem1.jpg" alt="Imagem postagem 1">
+               <span class="data-postagem">Postado em 10 de Março de 2022</span>
+               <p>
+                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur nihil ea neque dolorem provident voluptatibus eaque quas, dolores quae voluptatem eum reprehenderit atque hic asperiores praesentium, totam cumque tempora iure.
+               </p>
+               <a href="#">Leia Mais</a>
+           </div>
+           <!-- Fim Postagem 1 -->
+           <div class="postagens">
+               <h2>Curso De Redação: Aula - 1</h2>
+               <img src="img/imagem1.jpg" alt="Imagem postagem 1">
+               <span class="data-postagem">Postado em 10 de Março de 2022</span>
+               <p>
+                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur nihil ea neque dolorem provident voluptatibus eaque quas, dolores quae voluptatem eum reprehenderit atque hic asperiores praesentium, totam cumque tempora iure.
+               </p>
+               <a href="#">Leia Mais</a>
+           </div>
+           <!-- Fim Postagem 1 -->
+       </div>
+        <!-- Fim Área Postagens -->
+       <div class="area-lateral">
+           <div class="conteudo-lateral">
+               <h3 class="">Postagens Recentes</h3>
+               <p>
+                   Os Simpsons e as "Previsões sobre o futuro"...
+               </p>
+               <a href="#">Leia Mais</a>
+        
+               <p>
+                   Os Simpsons e as "Previsões sobre o futuro"...
+               </p>
+               <a href="#">Leia Mais</a>
+           </div>
+
+           <div class="conteudo-lateral">
+                <h3>Categorias</h3>
+                <a href="index.php">Home</a>
+                <a href="jogos.php">Jogos</a>
+                <a href="celulares.php">Celulares</a>
+                <a href="informatica.php">Informática</a>
+                <a href="eletrônicos.php">Eletrônicos</a>
+           </div>
+       </div>
+       <!-- Fim Área Lateral -->
+    </div>
+    <!-- Fim Área Principal -->
+    <div class="rodape">
+        <p>Todos os direitos reservados</p>
+    </div>
+</html>
